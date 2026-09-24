d1d21359d1630d4e6d4ce2aed5ac0e12c5bf3d14
Firdaus <firdausabomeh@gmail.com>
2026-09-24 19:26:43 +0200

    Ejercicio 1 probando Dart

---
+++ Relacion1/1holaMundo.dart
@@ -0,0 +1,11 @@
+/* Ejercicio 1 de la relacion 1
+    archivo: 1hola-mundo.dart
+    fecha creación: septiembre 2026
+    autora: Firdaus */
+
+void main(){
+  String nombre = 'Firdaus'; //Tipo y nombre de la variable
+  int veces = 1; 
+  print ('Hola $nombre!');
+  print ('Has ejecutado el programa ${veces*2-1} vez(es)');
+}
